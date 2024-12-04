f543aa9546fe41591b064da1072ba47efc7b30e3
JuannDarley <juandarley99@gmail.com>
2024-12-03 21:28:35 -0300

    created e2e tests for summary and transactions

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { it, beforeAll, afterAll, describe, expect } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
+import { execSync } from 'child_process'
 
 describe('Transactions routes', () => {
 	beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
 	afterAll(async () => {
 		await app.close()
+	})
+
+	beforeEach(() => {
+		execSync('npm run knex migrate:rollback --all')
+		execSync('npm run knex migrate:latest')
 	})
 
 	it('should be able to create a new transaction', async () => {
@@ -22,7 +28,7 @@ describe('Transactions routes', () => {
 			.expect(201)
 	})
 
-	it('should be able to list all transactions', async () => {
+	it('should be able to get especific transactions', async () => {
 		const createTransactionsResponse = await request(app.server)
 			.post('/transactions')
 			.send({
@@ -45,5 +51,70 @@ describe('Transactions routes', () => {
 				amount: 5000,
 			}),
 		])
+	})
+
+	it('should be able to list all transactions', async () => {
+		const createTransactionsResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New transactions',
+				amount: 5000,
+				type: 'credit',
+			})
+
+		const cookies = createTransactionsResponse.get('Set-Cookie')
+
+		const lsitTransactionsResponse = await request(app.server)
+			.get('/transactions')
+			//@ts-expect-error: The error occurs because TypeScript cannot guarantee that the passed value is not undefined
+			.set('Cookie', cookies)
+			.expect(200)
+
+		const transactionId = lsitTransactionsResponse.body.transactions[0].id
+
+		const getTransactionsResponse = await request(app.server)
+			.get(`/transactions/${transactionId}`)
+			//@ts-expect-error: The error occurs because TypeScript cannot guarantee that the passed value is not undefined
+			.set('Cookie', cookies)
+			.expect(200)
+
+		expect(getTransactionsResponse.body.transaction).toEqual(
+			expect.objectContaining({
+				title: 'New transactions',
+				amount: 5000,
+			}),
+		)
+	})
+
+	it('should be able to get the summary', async () => {
+		const createTransactionsResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'Credit transaction',
+				amount: 5000,
+				type: 'credit',
+			})
+
+		const cookies = createTransactionsResponse.get('Set-Cookie')
+
+		await request(app.server)
+			.post('/transactions')
+			//@ts-expect-error: The error occurs because TypeScript cannot guarantee that the passed value is not undefined
+			.set('Cookie', cookies)
+			.send({
+				title: 'Debit transaction',
+				amount: 2000,
+				type: 'debit',
+			})
+
+		const summaryResponse = await request(app.server)
+			.get('/transactions/summary')
+			//@ts-expect-error: The error occurs because TypeScript cannot guarantee that the passed value is not undefined
+			.set('Cookie', cookies)
+			.expect(200)
+
+		expect(summaryResponse.body.summary).toEqual({
+			amount: 3000,
+		})
 	})
 })
